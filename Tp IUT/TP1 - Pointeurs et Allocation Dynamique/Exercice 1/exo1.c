bdfbb0308d226367bc0d9091462a6bb8e0c723c3
Alexandre Kotowicz <Alexandre.kcz.pro@gmail.com>
2022-09-06 09:27:06 +0200

    modified:   Tp IUT/TP1 - Pointeurs et Allocation Dynamique/Exercice 1/exo1.c

--- Tp IUT/TP1 - Pointeurs et Allocation Dynamique/Exercice 1/exo1.c
+++ Tp IUT/TP1 - Pointeurs et Allocation Dynamique/Exercice 1/exo1.c
@@ -7,6 +7,6 @@ int *p[10];         // Declare un tableau de 10 pointeurs vers des entiers
 int (*p)[10];       // Declare un pointeur vers un Tableau d'entier
 char *p();          // Declare un pointeur vers une fonction P
 char (*p)();        //
-int (*p)(char *a);  //
+int (*p)(char *a);  // Declare
 
 char p() { return ""; }
